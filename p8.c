10183500986e7cd2c50efae123d02aeb3dcd9e80
brtanish <brt.anishlap@gmail.com>
2025-02-10 20:57:48 +0545

    Create p8.c

---
+++ p8.c
@@ -0,0 +1,13 @@
+/* c program to check whether number is even or odd*/
+#include <stdio.h>
+int main()
+{
+    int num;
+    printf("Enter an integer number: ");
+    scanf("%d",&num);
+    if(num%2==0)
+        printf("%d is an EVEN number.",num);
+    else
+        printf("%d is an ODD number.",num);
+    return 0;
+}
