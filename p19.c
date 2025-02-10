ee40eae7df51b2701087a09c37786284ab0ef1fe
brtanish <brt.anishlap@gmail.com>
2025-02-10 21:59:47 +0545

    Create p19.c

---
+++ p19.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    float l,a,b;
+    printf("Enter length and breadth:");
+    scanf("%f%f",&l,&b);
+    a=l*b;
+    printf("The area of a rectangle is %f",a);
+    return 0;
+}
