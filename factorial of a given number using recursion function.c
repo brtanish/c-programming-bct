94f6fa6ee00da5336f4d2305a188716b6af2d10c
brtanish <brt.anishlap@gmail.com>
2025-02-16 21:16:00 +0545

    Create factorial of a given number using recursion function.c

---
+++ factorial of a given number using recursion function.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int factorial(int num) {
+    if (num == 0 || num == 1) {
+        return 1;
+    }
+    return num * factorial(num - 1);
+}
+
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    printf("Factorial of %d is: %d\n", num, factorial(num));
+
+    return 0;
+}
